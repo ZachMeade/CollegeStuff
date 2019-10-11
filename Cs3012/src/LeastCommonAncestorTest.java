8ef289e02ad68ef6b632dd4e678542087b4381bf
meadez@tcd.ie <meadez@tcd.ie>
2019-10-11 16:42:23 +0100

    added more tests for code coverage

--- Cs3012/src/LeastCommonAncestorTest.java
+++ Cs3012/src/LeastCommonAncestorTest.java
@@ -54,6 +54,34 @@ public class LeastCommonAncestorTest {
         assertNull(test.search(test.root,5,5));
         assertNull(test.search(test.root,20,1));
     }
+    @Test // test when v1 and v2 are the same value
+    public void testSameValue()
+    {
+        LeastCommonAncestor<Integer> test = new LeastCommonAncestor<Integer>();
+        test.put(9);
+        test.put(1);
+        test.put(4);
+        test.put(19);
+        test.put(9);
+        test.put(13);
+        assertSame(test.search(test.root,9,9).key,9);
+        assertSame(test.search(test.root,19,19).key,19);
+    }
+
+    @Test
+    public void testGet()
+    {
+        LeastCommonAncestor<Integer> bst = new LeastCommonAncestor<Integer>();
+        assertNull("test get empty tree", bst.get(7));
+        bst.put(1);
+        assertSame("test single tree", 1, bst.get(1).key);
+        bst.put(2);
+        bst.put(4);
+        bst.put(5);
+        assertSame("test value larger than root",4, bst.get(4).key);
+        assertSame("test value smaller than root", 2, bst.get(2).key);
+        assertSame("test value larger but not in tree", null, bst.get(11));
+    }
 
 
 }
